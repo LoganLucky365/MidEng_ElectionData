9996c6f292d3238cb8aab88ce713f4f313e2845c
Sebastian Profous <s.profous@student.tgm.ac.at>
2024-10-08 13:36:13 +0200

    Creating the Spring Project with Maven

---
+++ src/main/java/com/example/mideng_election_data/MidEngElectionDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.mideng_election_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidEngElectionDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidEngElectionDataApplication.class, args);
+	}
+
+}
